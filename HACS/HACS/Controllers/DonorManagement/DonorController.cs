8960b6b3218f508cc80d29d6d73a6ad226ec4dc0
Blazej Dymek <dymekblazej@outlook.com>
2024-12-15 22:41:29 +0100

    Added basic functions to update and delete a donor

--- HACS/HACS/Controllers/DonorManagement/DonorController.cs
+++ HACS/HACS/Controllers/DonorManagement/DonorController.cs
@@ -19,15 +19,13 @@ public class DonorController : ControllerBase
         _donorRepo = donorRepo;
         _donationRepo = donationRepo;
     }
-    
     [HttpGet]
     public async Task<IActionResult> GetAll()
     {
         var donors = await _donorRepo.GetAllAsync();
         return Ok(donors);
     }
-    
-    [HttpGet("{id}")]
+    [HttpGet("{id:guid}")]
     public async Task<IActionResult> GetById([FromRoute] Guid id)
     {
         var donor = await _donorRepo.GetByIdAsync(id);
@@ -38,11 +36,24 @@ public class DonorController : ControllerBase
         }
         return Ok(donor);
     }
-    
     [HttpPost]
     public async Task<IActionResult> Create([FromBody] Donor donor)
     {
         await _donorRepo.CreateAsync(donor);
         return CreatedAtAction(nameof(GetById), new { id = donor.Id }, donor);
+    }
+    [HttpPut]
+    public async Task<IActionResult> Update([FromBody] Donor donor)
+    {
+        // TODO: Add and remove donations from the donation history / create DTOs
+        await _donorRepo.UpdateAsync(donor);
+        return NoContent();
+    }
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> Delete([FromRoute] Guid id)
+    {
+        // TODO: Remove donations on delete
+        await _donorRepo.DeleteAsync(id);
+        return NoContent();
     }
 }
