ebe871ba4faf136676540c6c59daacabe4aea177
Błażej Dymek <dymekblazej@outlook.com>
2025-01-19 01:37:37 +0100

    added login

--- HACS/HACS/Controllers/DonorController.cs
+++ HACS/HACS/Controllers/DonorController.cs
@@ -1,8 +1,14 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using HACS.Dtos;
 using HACS.Dtos.Donor;
 using HACS.Interfaces;
 using HACS.Mappers;
 using HACS.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace HACS.Controllers;
 
@@ -11,10 +17,43 @@ namespace HACS.Controllers;
 public class DonorController : ControllerBase
 {
     private readonly IRepository<Donor> _donorRepo;
+    private readonly UserManager<DonationAdmin> _userManager;
+    private readonly IConfiguration _config;
     
-    public DonorController(IRepository<Donor> donorRepo)
+    public DonorController(IConfiguration config, IRepository<Donor> donorRepo, UserManager<DonationAdmin> userManager)
     {
         _donorRepo = donorRepo;
+        _userManager = userManager;
+        _config = config;
+    }
+    
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+    {
+        var user = await _userManager.FindByNameAsync(loginDto.Username);
+        if (user is null || !await _userManager.CheckPasswordAsync(user, loginDto.Password) || user.UserName is null) return Unauthorized();
+        var claims = new List<Claim>
+        {
+            new (JwtRegisteredClaimNames.Sub, user.Id),
+            new (JwtRegisteredClaimNames.UniqueName, user.UserName),
+            new (JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+        var roles = await _userManager.GetRolesAsync(user);
+        claims.AddRange(roles.Select(r => new Claim(ClaimTypes.Role, r)));
+        
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:Secret"]));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        var token = new JwtSecurityToken(
+            issuer: _config["JWT:ValidIssuer"],
+            audience: _config["JWT:ValidAudience"],
+            claims: claims,
+            expires: DateTime.Now.AddDays(1),
+            signingCredentials: creds);
+        
+        return Ok(new
+        {
+            token = new JwtSecurityTokenHandler().WriteToken(token)
+        });
     }
 
     [HttpGet]
